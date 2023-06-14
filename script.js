fdd49332b9dedb6fb6fa98d8873b5c9d97ba60dd
Renata Veras Venturim <129551549+RenataVerasVenturim@users.noreply.github.com>
2023-06-14 00:20:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+/*JS Document*/
+
+/* Objetivo: Enviar e mail para Renata*/
+
+/* Declaração das variáveis */
+var mensagem;
+var mailtolink;  
+
+/* Entrada de dados */
+var mensagem = document.getElementById('caixadetexto').value;
+
+
+/* Saída de dados */
+function EnviarEmail(){
+    window.alert('Em construção');
+    
+}
+
+
+
+
+  
